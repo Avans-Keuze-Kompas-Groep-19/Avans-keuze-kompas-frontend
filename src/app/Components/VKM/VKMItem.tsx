8d09820b1cad72af3ae4f8db0f15da67d7c30e94
jarivankaam <jari@jarivankaam.nl>
2025-12-15 12:25:14 +0100

    front-end | sidebarFilters | add checkbox and ui library

--- src/app/Components/VKM/VKMItem.tsx
+++ src/app/Components/VKM/VKMItem.tsx
@@ -11,7 +11,7 @@ export default function ItemsView() {
     if (loading) return <p>Loading...</p>;
 
     return (
-        <div className="container flex">
+        <div className="container flex gap-2">
             <div className="col">
                 <Sidebar />
             </div>
