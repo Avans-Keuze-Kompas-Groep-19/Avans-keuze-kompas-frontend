2660187add6a205fc4a9cb528fffb8466e1b4ad4
jarivankaam <jari@jarivankaam.nl>
2026-01-05 11:46:47 +0100

    and add hero

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import UserList from "@/app/Components/Users/UserList";
 import { Header } from "../Components/layout/header/Header";
+import HeroSection from "../Components/layout/Hero/Hero";
 
 const UsersPage = () => {
   return (
     <>
       <Header />
       <main className="flex flex-col justify-center items-center m-10">
+        <HeroSection
+          heading="Bewerk hier alle gebruikers!"
+          description=""
+          imageSrc="/hero.png"
+          imageAlt="Hero image"
+          background="#c6002a"
+        />
         <div>
           <h1 className="text-2xl font-bold mb-4">All Users</h1>
           <UserList />
