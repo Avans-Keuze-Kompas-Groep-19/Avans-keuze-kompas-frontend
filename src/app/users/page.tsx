d5d7173694d21906075fb68057a49f5eb1937fb5
jarivankaam <jari@jarivankaam.nl>
2026-01-05 12:24:54 +0100

    add login check

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,27 +1,23 @@
-import React from "react";
+"use client";
+
+import React from 'react';
 import UserList from "@/app/Components/Users/UserList";
-import { Header } from "../Components/layout/header/Header";
-import HeroSection from "../Components/layout/Hero/Hero";
+import { useAuth } from "@/app/lib/auth/useAuth";
+import LoginForm from "@/app/Components/Auth/loginForm";
+import { Header } from "@/app/Components/layout/header/Header";
 
 const UsersPage = () => {
-  return (
-    <>
-      <Header />
-      <main className="flex flex-col justify-center m-10">
-        <HeroSection
-          heading="Bewerk hier alle gebruikers!"
-          description=""
-          imageSrc="/hero.png"
-          imageAlt="Hero image"
-          background="#c6002a"
-        />
-        <div>
-          <h1 className="text-2xl font-bold mb-4">All Users</h1>
-          <UserList />
-        </div>
-      </main>
-    </>
-  );
+    const { isLoggedIn } = useAuth();
+
+    return (
+        <>
+            <Header />
+            <main className="p-6 max-w-4xl mx-auto">
+                <h1 className="text-3xl font-bold mb-6">Users Dashboard</h1>
+                {isLoggedIn ? <UserList /> : <LoginForm />}
+            </main>
+        </>
+    );
 };
 
 export default UsersPage;
