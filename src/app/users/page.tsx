b65151193e2d22d54ec25cd7fa6df443a593e705
jarivankaam <jari@jarivankaam.nl>
2026-01-05 11:45:38 +0100

    add header

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
 import UserList from "@/app/Components/Users/UserList";
+import { Header } from "../Components/layout/header/Header";
 
 const UsersPage = () => {
-    return (
+  return (
+    <>
+      <Header />
+      <main className="flex flex-col justify-center items-center m-10">
         <div>
-            <h1 className="text-2xl font-bold mb-4">All Users</h1>
-            <UserList />
+          <h1 className="text-2xl font-bold mb-4">All Users</h1>
+          <UserList />
         </div>
-    );
+      </main>
+    </>
+  );
 };
 
 export default UsersPage;
