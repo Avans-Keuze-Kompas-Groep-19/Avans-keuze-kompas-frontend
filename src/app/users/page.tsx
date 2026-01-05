077c945f4dcc5395d6fade1682f3565d66d83e5e
jarivankaam <jari@jarivankaam.nl>
2026-01-05 12:37:00 +0100

    add hero back

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,23 +1,35 @@
 "use client";
 
-import React from 'react';
+import React from "react";
 import UserList from "@/app/Components/Users/UserList";
 import { useAuth } from "@/app/lib/auth/useAuth";
 import LoginForm from "@/app/Components/Auth/loginForm";
 import { Header } from "@/app/Components/layout/header/Header";
+import HeroSection from "../Components/layout/Hero/Hero";
 
 const UsersPage = () => {
-    const { isLoggedIn } = useAuth();
+  const { isLoggedIn } = useAuth();
 
-    return (
-        <>
-            <Header />
-            <main className="p-6 max-w-4xl mx-auto">
-                <h1 className="text-3xl font-bold mb-6">Users Dashboard</h1>
-                {isLoggedIn ? <UserList /> : <LoginForm />}
-            </main>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <HeroSection
+        heading={
+          isLoggedIn
+            ? "Bewerk hier de gebruikers!"
+            : "Log in om gebruikers te bewerken"
+        }
+        description=""
+        imageSrc="/hero.png"
+        imageAlt="Hero image"
+        background="#c6002a"
+      />
+      <main className="p-6 max-w-4xl mx-auto">
+        <h1 className="text-3xl font-bold mb-6">Users Dashboard</h1>
+        {isLoggedIn ? <UserList /> : <LoginForm />}
+      </main>
+    </>
+  );
 };
 
 export default UsersPage;
